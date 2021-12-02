53152e1f0f0d583a384ce98b230aceed7f52b4d5
Joe Maurer <joe@maurer.gg>
2021-12-02 09:40:36 -0500

    fix naming convenctions, remove extraneous files

---
+++ src/pages/EditExercisePage.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
+
+export const EditExercisePage = () => {
+
+    return (
+        <div>
+            <h1>Edit Exercise</h1>
+        </div>
+    );
+}
+
+export default EditExercisePage;
